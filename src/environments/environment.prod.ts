27ccb486e8cc7386d04b4ff20b9f6f30330a8d28
TibiAlex <tibi.alex@yahoo.com>
2023-04-03 18:48:01 +0300

    id2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  PATH_BASE_URL: 'load-balancer-950787169.us-east-1.elb.amazonaws.com:4000/api'
+  PATH_BASE_URL: 'load-balancer-950787169.us-east-1.elb.amazonaws.com/api'
 };
